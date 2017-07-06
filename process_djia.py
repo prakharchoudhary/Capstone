bd339caa6360158e0e2a9a4414188817589b51e3
prakharchoudhary <prakhar2397@gmail.com>
2017-07-06 15:33:55 +0530

    add sentiment analysis model for tagging of tweets

--- process_djia.py
+++ process_djia.py
@@ -3,4 +3,9 @@ TODO:
 
 1. fill in interpolated data for missing dates(eg. sundays, holidays etc.)
 """
+import csv
 
+with open('DJIA.csv', 'rb') as csvfile:
+	reader = csv.reader(csvfile)
+	for row in reader:
+		print row
