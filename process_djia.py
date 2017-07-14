ea4d560e926cf455e152ded9bc3d392ce8301b14
prakharchoudhary <prakhar2397@gmail.com>
2017-07-14 23:32:38 +0530

    add interpolation function for filling missing data

--- process_djia.py
+++ process_djia.py
@@ -1,11 +1,55 @@
 """
 TODO:
 
-1. fill in interpolated data for missing dates(eg. sundays, holidays etc.)
-"""
-import csv
+-> fill in interpolated data for missing dates(eg. sundays, holidays etc.)
 
-with open('DJIA.csv', 'rb') as csvfile:
-	reader = csv.reader(csvfile)
-	for row in reader:
-		print row
+"""
+
+import pandas as pd
+import datetime
+
+def load_file(filename):
+	
+	f = open(filename, "rb")
+	df = pd.read_csv(f)
+	# Now change the date in usable format
+	dates = []
+	for i in df['Date']:
+		n = i.split('/')
+		n = "{}-{}-20{}".format(n[2], n[0], n[1])
+		dates.append(n)
+	df['Date'] = dates
+	f.close()
+
+def concave(x, y, n):
+
+	prices = []
+	prices.append(x)
+	print prices
+	for i in range(n):
+		new_price  = (y+prices[i])/2
+		prices.append(new_price)
+		print "{}. ----> {:.2f}".format(i+1, new_price)
+
+
+def get_all_dates(start_date, end_date):
+	
+	dates = []
+	start_date = datetime.datetime.strptime(start_date, "%Y-%m-%d")
+	end_date = datetime.datetime.strptime(end_date, "%Y-%m-%d")
+	step = timedelta(days=1)
+	while start_date <= end_date:
+		dates.append(str(start_date.date()))
+		start_date += step
+
+	return dates
+
+def dates_iter(df):
+
+	l = len(df.Date)
+	all_dates = get_all_dates(df.Date[0], df.Date[l-1])
+
+
+if __name__ == '__main__':
+	
+	main()
